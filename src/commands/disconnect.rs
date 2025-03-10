966200657fb3161fdf2c6b339d1c049a06845214
nblockbuster <34419030+nblockbuster@users.noreply.github.com>
2025-03-10 14:10:17 -0400

    stupid

---
+++ src/commands/disconnect.rs
@@ -0,0 +1,37 @@
+use log::{error, info, warn};
+use serenity::builder::*;
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+use songbird::TrackEvent;
+
+use crate::{COLOR_OK, UserData};
+
+pub fn register() -> CreateCommand {
+    CreateCommand::new("disconnect")
+        .description("Disconnect the bot from the actively playing voice channel")
+}
+
+pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<(), serenity::Error> {
+    let guild_id = interaction.guild_id.unwrap();
+    let mut typemap = ctx.data.write().await;
+    let data = typemap.get_mut::<UserData>().unwrap();
+    let track = data.track_handles.get_mut(&guild_id).unwrap();
+
+    let _ = track.stop();
+    let _ = data.songbird.remove(guild_id).await;
+
+    interaction
+        .create_response(
+            ctx,
+            CreateInteractionResponse::Message(
+                CreateInteractionResponseMessage::new().embed(
+                    CreateEmbed::new()
+                        .color(Colour::new(COLOR_OK))
+                        .description("Stopped track"),
+                ),
+            ),
+        )
+        .await?;
+
+    Ok(())
+}
---
+++ src/commands/pause.rs
@@ -0,0 +1,70 @@
+use log::{error, info, warn};
+use serenity::builder::*;
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+use songbird::{TrackEvent, tracks::PlayMode};
+
+use crate::{COLOR_ERROR, COLOR_OK, UserData};
+
+pub fn register() -> CreateCommand {
+    CreateCommand::new("pause").description("Pause/plays the active music")
+}
+
+pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<(), serenity::Error> {
+    let guild_id = interaction.guild_id.unwrap();
+    let mut typemap = ctx.data.write().await;
+    let data = typemap.get_mut::<UserData>().unwrap();
+    let track = data.track_handles.get_mut(&guild_id).unwrap();
+
+    let info = track.get_info().await.unwrap();
+    match info.playing {
+        PlayMode::Pause => {
+            let _ = track.play();
+            interaction
+                .create_response(
+                    ctx,
+                    CreateInteractionResponse::Message(
+                        CreateInteractionResponseMessage::new().embed(
+                            CreateEmbed::new()
+                                .color(Colour::new(COLOR_OK))
+                                .description("Resuming track"),
+                        ),
+                    ),
+                )
+                .await?;
+        }
+        PlayMode::Play => {
+            let _ = track.pause();
+            interaction
+                .create_response(
+                    ctx,
+                    CreateInteractionResponse::Message(
+                        CreateInteractionResponseMessage::new().embed(
+                            CreateEmbed::new()
+                                .color(Colour::new(COLOR_OK))
+                                .description("Pausing track"),
+                        ),
+                    ),
+                )
+                .await?;
+        }
+        _ => {
+            interaction
+                .create_response(
+                    ctx,
+                    CreateInteractionResponse::Message(
+                        CreateInteractionResponseMessage::new().embed(
+                            CreateEmbed::new()
+                                .color(Colour::new(COLOR_ERROR))
+                                .description("Could not pause or play track")
+                                .title("Error")
+                                .timestamp(Timestamp::now()),
+                        ),
+                    ),
+                )
+                .await?;
+        }
+    }
+
+    Ok(())
+}
